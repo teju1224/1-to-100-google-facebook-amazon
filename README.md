c7f29ed13d2110c2d5b5f8ad6ee81ae9d01e719a
teju1224 <91451141+teju1224@users.noreply.github.com>
2023-02-13 23:18:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 1-to-100-google-facebook-amazon
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-q8vnfb)
---
+++ index.js
@@ -0,0 +1,4 @@
+for (var i = 1; i <= 100; i++) {
+  var f = i % 3 == 0, b = i % 5 == 0;
+  console.log(f ? b ? "amazon" : "google" : b ? "facebook" : i);
+}
